06dcb6e2e41caa3ee13df631c0ef83f23c059ec3
SherryHan <qimenghan77@gmail.com>
2017-10-10 12:39:43 -0400

    Create event and timesolts table

--- manage_event/models.py
+++ manage_event/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Events(models.Model):
     event_name = models.CharFiled(max_length=300)
+    event_organizer = models.CharField(max_length=300)
 
 class TimeSlots(model.Model):
-    timesolts_date = models.TimeField()
+    evetn_id = models.IntegerField(primary_key=True)
+    event_participants = models.CharField(primary_key=True)
+    timesolts_date = models.DateTimeField(None, None,False, False)
