0af18842620b46d20ad4f46821cb890f39c8cbe8
SherryHan <qimenghan77@gmail.com>
2017-10-11 01:35:08 -0400

    Create URLS

--- manage_event/models.py
+++ manage_event/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Events(models.Model):
     event_name = models.CharFiled(max_length=300)
     event_organizer = models.CharField(max_length=300)
+    event_date = models.DateTimeField(None, None, False, False)
 
 class TimeSlots(model.Model):
-    evetn_id = models.IntegerField(primary_key=True)
+    #two fields are primary key together
+    event_id = models.IntegerField(primary_key=True)
     event_participants = models.CharField(primary_key=True)
     timesolts_date = models.DateTimeField(None, None,False, False)
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from home import views
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
+    url(r'^manage_event/', include('manage_event.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index)
 ]
