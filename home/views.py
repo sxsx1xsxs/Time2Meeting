92f889bc63e83a1d85ac14624657967bc70c1103
Shadow <shadow.lzd@gmail.com>
2017-11-11 00:48:02 -0500

    minor change

--- home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def guidance(request):
-    return render(request, 'home/guidance.html')
-
-
-def team(request):
-    return render(request, 'home/team.html')
