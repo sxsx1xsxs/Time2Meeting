0ef7bb8305f9dc596950cc61693244d014592873
Yi Zhang <yz3206@columbia.edu>
2017-11-15 21:31:32 -0500

    commit before pull

--- manage_event/models.py
+++ manage_event/models.py
@@ -5,15 +5,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-# # Create your models here.
-# class Users(models.Model):
-#     user_email = models.EmailField(max_length=254, primary_key=True)
-#     user_name = models.CharField(max_length=300)
-#
-#     def __str__(self):
-#         return self.user_name
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, unique=True, null=False, db_index=True)
     bio = models.TextField(max_length=500, blank=True)
@@ -33,7 +24,6 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
 
 
 class Events(models.Model):
