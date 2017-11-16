bc3208bf8854163f33e7d420e87d7469747dbdb1
Yi Zhang <yz3206@columbia.edu>
2017-11-15 22:12:52 -0500

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -167,7 +167,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
-        'PORT': '5431',
+        'PORT': '5432',
         'TEST': {
             'NAME': 'mytestdatabase',
         },
