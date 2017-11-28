578864edd80395357ffc2a54f97d598196974e55
Yi Zhang <yz3206@columbia.edu>
2017-11-28 14:10:56 -0500

    seperate js files

--- mysite/settings.py
+++ mysite/settings.py
@@ -54,7 +54,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'http://storage.googleapis.com/time2meeting-184004/static/'
+# STATIC_URL = 'http://storage.googleapis.com/time2meeting-184004/static/'
+
+STATIC_URL = '/static/'
 
 STATIC_ROOT = 'static/'
 
