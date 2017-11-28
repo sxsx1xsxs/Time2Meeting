49e6ca5dc55f863a279e1c6bbf502a06e0cfe5b6
Yi Zhang <yz3206@columbia.edu>
2017-11-28 17:26:32 -0500

    add dummy javascript test case and jasmine javascript test framework

---
+++ spec/javascripts/manage_event/timeslots_spec.js
@@ -0,0 +1,8 @@
+describe("Test timeslots11.js", function() {
+  it("test variable test to be 1", function(){
+
+      var x = TestVariable;
+      expect(x).toBeDefined();
+      expect(x).toEqual(1);
+  });
+});
