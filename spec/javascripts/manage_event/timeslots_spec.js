f2dac098870a6d874eb7d43b18a66e6cb83e791f
Yi Zhang <yz3206@columbia.edu>
2017-11-29 15:31:00 -0500

    add some more real test cases for timeslots.js and improve the calendar ui by center aligning text

--- spec/javascripts/manage_event/timeslots_spec.js
+++ spec/javascripts/manage_event/timeslots_spec.js
@@ -8,4 +8,19 @@ describe("Test timeslots.js", function() {
   it("test orginalTimeSlotsToBeUndefined", function(){
     expect(originalTimeslots).toBeUndefined();
   })
+
+  it("test dayHalfHoursGeneratorHelper", function(){
+    var times = dayHalfHoursGeneratorHelper();
+    expect(times[0]).toEqual("00:00:00");
+    expect(times[23]).toEqual("11:30:00");
+    expect(times[47]).toEqual("23:30:00");
+  })
+
+  it("test timeFormat", function(){
+    expect(timeFormat("00")).toEqual(" 0AM");
+    expect(timeFormat("09")).toEqual(" 9AM");
+    expect(timeFormat("10")).toEqual("10AM");
+    expect(timeFormat("20")).toEqual(" 8PM");
+    expect(timeFormat("22")).toEqual("10PM");
+  })
 });
