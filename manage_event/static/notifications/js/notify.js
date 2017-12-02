902e66275f0dd4cabc2f9f06d7732ec07f471c89
Yufeng Zhang <waynezhang511@gmail.com>
2017-12-01 19:55:36 -0500

    Add feature: notification center. (Abort and decide supported)

---
+++ manage_event/static/notifications/js/notify.js
@@ -0,0 +1,102 @@
+var notify_badge_id;
+var notify_menu_id;
+var notify_api_url;
+var notify_fetch_count;
+var notify_unread_url;
+var notify_mark_all_unread_url;
+var notify_refresh_period = 15000;
+var consecutive_misfires = 0;
+var registered_functions = [];
+
+function fill_notification_badge(data) {
+    var badge = document.getElementById(notify_badge_id);
+    if (badge) {
+        badge.innerHTML = data.unread_count;
+    }
+}
+
+console.log('hi');
+
+function fill_notification_list(data) {
+    var menu = document.getElementById(notify_menu_id);
+    if (menu) {
+        var content = [];
+        menu.innerHTML = data.unread_list.map(function (item) {
+            var message = "";
+            if(typeof item.actor !== 'undefined'){
+                message = item.actor;
+            }
+            if(typeof item.verb !== 'undefined'){
+                message = message + " " + item.verb;
+            }
+            if(typeof item.target !== 'undefined'){
+                message = message + " " + item.target;
+            }
+            if(typeof item.timestamp !== 'undefined'){
+                message = message + " " + item.timestamp;
+            }
+            return '<li>' + message + '</li>';
+        }).join('')
+    }
+}
+
+function fill_notification_list_with_link(data) {
+    var menu = document.getElementById(notify_menu_id);
+    if (menu) {
+        var content = [];
+        menu.innerHTML = data.unread_list.map(function (item) {
+            var message = "";
+            if(typeof item.actor !== 'undefined'){
+                message = item.actor;
+            }
+            if(typeof item.verb !== 'undefined'){
+                message = message + " " + item.verb;
+            }
+            if(typeof item.target !== 'undefined'){
+                message = message + " " + "<b>" + item.target + "</b>";
+            }
+            if(typeof item.timestamp !== 'undefined'){
+                message = message + " on " + item.timestamp;
+            }
+            if(item.description !== null){
+                message = '<a href="' + item.description + '/notification_redirect/' + item.id +  '">' + message + '</a>';
+            }
+            return '<li>' + message + '</li>';
+        }).join('')
+    }
+}
+
+function register_notifier(func) {
+    registered_functions.push(func);
+}
+
+function fetch_api_data() {
+    if (registered_functions.length > 0) {
+        //only fetch data if a function is setup
+        var r = new XMLHttpRequest();
+        r.addEventListener('readystatechange', function(event){
+            if (this.readyState === 4){
+                if (this.status === 200){
+                    consecutive_misfires = 0;
+                    var data = JSON.parse(r.responseText);
+                    registered_functions.forEach(function (func) { func(data); });
+                }else{
+                    consecutive_misfires++;
+                }
+            }
+        })
+        r.open("GET", notify_api_url+'?max='+notify_fetch_count, true);
+        r.send();
+    }
+    if (consecutive_misfires < 10) {
+        setTimeout(fetch_api_data,notify_refresh_period);
+    } else {
+        var badge = document.getElementById(notify_badge_id);
+        if (badge) {
+            badge.innerHTML = "!";
+            badge.title = "Connection lost!"
+        }
+    }
+}
+
+setTimeout(fetch_api_data, 1000);
--- mysite/settings.py
+++ mysite/settings.py
@@ -52,7 +52,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-#STATIC_URL = 'http://storage.googleapis.com/time2meeting-184004/static/'
+# STATIC_URL = 'http://storage.googleapis.com/time2meeting-184004/static/'
 
 STATIC_URL = '/static/'
 
