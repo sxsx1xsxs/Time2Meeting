1c45c9f11d8e1a7fb9bafffd0aefb4325b23c4f4
SherryHan <qimenghan77@gmail.com>
2017-12-12 02:46:37 -0500

    email validation: prevent sending email to oneself

--- manage_event/views.py
+++ manage_event/views.py
@@ -330,7 +330,7 @@ def create_publish(request, event_id):
     event = get_object_or_404(Events, pk=event_id)
 
     if request.method == 'POST':
-        form = InvitationForm(request.POST)
+        form = InvitationForm(data = request.POST, user_obj = request.user)
         if form.is_valid():
             mail_list = form.cleaned_data
             objs = Invitation.create(event=event, mail_list=mail_list, inviter=request.user)
@@ -339,7 +339,7 @@ def create_publish(request, event_id):
             messages.success(request, _('Invite Success!'))
             return HttpResponseRedirect(reverse('manage_event:create_publish', args=(event_id,)))
     else:
-        form = InvitationForm()
+        form = InvitationForm(user_obj = request.user)
     return render(request, 'manage_event/create_publish.html', {'event_id': event_id, 'form': form})
 
 
@@ -423,7 +423,7 @@ def on_going(request, event_id):
     """
     event = get_object_or_404(Events, pk=event_id)
     if request.method == 'POST':
-        form = InvitationForm(request.POST)
+        form = InvitationForm(data = request.POST,user_obj = request.user)
         if form.is_valid():
             mail_list = form.cleaned_data
             objs = Invitation.create(event=event, mail_list=mail_list, inviter=request.user)
@@ -432,7 +432,7 @@ def on_going(request, event_id):
             messages.success(request, _('Invite Success!'))
             return HttpResponseRedirect(reverse('manage_event:on_going', args=(event_id,)))
     else:
-        form = InvitationForm()
+        form = InvitationForm(user_obj = request.user)
     return render(request, 'manage_event/on_going.html', {'event': event, 'form': form})
 
 
